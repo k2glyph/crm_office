1feccf0e54aa1e566eb1d6b0b43ec91f7dcfa10c
mfzall07 <mfaisalhafid@gmail.com>
2022-03-03 08:32:08 +0700

    remove main explore

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -35,9 +35,6 @@
 @include('layout/topbar/partials/_activity-drawer')
 
 
-@include('layout/explore/_main')
-
-
 <!--begin::Chat drawer-->
 <div id="kt_drawer_chat" class="bg-body" data-kt-drawer="true" data-kt-drawer-name="chat" data-kt-drawer-activate="true"
 	data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'300px', 'md': '500px'}" data-kt-drawer-direction="end"
