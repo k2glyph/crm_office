69948505186dc6318e8ba2b46bb478e3366799ee
mfzall07 <mfaisalhafid@gmail.com>
2022-04-08 10:53:51 +0700

    fixed management folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,8 @@ import Dashboard from './Pages/Dashboard/Dashboard';
 function App() {
   return (
     <div>
-      <div className='flex flex-row'>
-        <Aside />
-        <div className='flex-1 p-6'>
-          <Dashboard />
-        </div>
+      <div>
+        <Dashboard />
       </div>
     </div>
   );
